3e44a89ab5c13e0903e3e09390c8e15b924d348a
Blake Brisben <blakebri@rams.colostate.edu>
2019-12-27 16:39:11 -0700

    FirstPython Commit 1

---
+++ FirstPython.py
@@ -0,0 +1,18 @@
+def FunctionyFunction(a, b):
+    return (a**2) + (b**2) ** .5
+
+for x in range(10):
+    print("%d" % x)
+
+if 1:
+    print("gosh darn thats true!")
+
+x = 0
+
+while x < 15:
+    print("x = %d" % x)
+    x += 1
+
+    print(FunctionyFunction(x,x+1))
+
+
